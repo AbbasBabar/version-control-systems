b30e72fb47a3b3d7ce71c0ac9201b874f89334a9
Babar Abbas <abbas.babar@gmail.com>
2009-06-29 05:35:31 +0600

    A Hello World Git Project

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+
+
+
+public class HelloWorld
+{
+	public void greetings()
+	{
+		System.out.println("Hello! every body");
+	}
+
+	public void greetingsAgain()
+	{
+		System.out.println("Hello! every body again");
+	}
+
+	public static void main(String[] args)
+	{
+		HelloWorld hello = new HelloWorld();
+		hello.greetings();
+		hello.greetingsAgain();
+	}
+}
